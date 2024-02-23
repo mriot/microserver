785a7d7e710b49734f053e63594ecf5ca4942207
Markus <kremer.markus1996@gmail.com>
2024-02-24 00:53:07 +0100

    condense code; cleanup

--- microserver.py
+++ microserver.py
@@ -3,7 +3,7 @@ import socket
 
 
 class MicroServer:
-    HTTP_STATUS = {
+    HTTP_CODES = {
         200: "OK",
         404: "Not Found",
         500: "Internal Server Error",
@@ -11,44 +11,34 @@ class MicroServer:
 
     def __init__(self, name="", port=8080):
         self.name = name
+        self.port = port
         self.connection = None
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._server_address = ("", port)
-        self._sock.bind(self._server_address)
+        self._sock.bind(("", port))
         self._sock.listen(1)
-
         self._routes = {
-            "/": lambda: {
-                "name": self.name,
-                "routes": list(self._routes.keys()),
-            }
+            "/": lambda: {"name": self.name, "routes": list(self._routes.keys())}
         }
 
     def add_route(self, path, callback):
         self._routes[path] = callback
 
-    def _send(self, data, status=200):
-        if self.connection is None:
-            return
-
-        content = json.dumps({"data" if status == 200 else "error": data})
-        self.connection.send(
-            f"HTTP/1.1 {status} {self.HTTP_STATUS[status]}\r\nContent-Type: application/json\r\n\r\n{content}".encode()
-        )
+    def _send(self, data, code=200):
+        if self.connection is not None:
+            content = json.dumps({"data" if code == 200 else "error": data})
+            self.connection.send(
+                f"HTTP/1.1 {code} {self.HTTP_CODES[code]}\r\nContent-Type: application/json\r\n\r\n{content}".encode()
+            )
 
     def start(self):
-        print(f"Server running on port {self._server_address[1]}...")
+        print(f"Server running on port {self.port}...")
         while True:
             self.connection, client_address = self._sock.accept()
-
             try:
-                data = self.connection.recv(1024)
-                lines = data.decode().split("\r\n")
-                method, route, _ = lines[0].split(" ")
-                print(
-                    f"{method} {route} on :{self._sock.getsockname()[1]} from {client_address[0]}"
-                )
+                request = self.connection.recv(1024).decode().split("\r\n")
+                method, route, _ = request[0].split(" ")
+                print(f"{method} {route} on :{self.port} from {client_address[0]}")
 
                 if route in self._routes:
                     self._send(self._routes[route]())
