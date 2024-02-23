38a872886d59b001af7c26e22a93599bef2cc19e
Markus <kremer.markus1996@gmail.com>
2024-02-23 03:17:51 +0100

    cleanup

--- microserver.py
+++ microserver.py
@@ -11,35 +11,32 @@ class MicroServer:
 
     def __init__(self, name, port=8080):
         self.name = name
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = ("", port)
-        self.sock.bind(self.server_address)
-        self.sock.listen(1)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._server_address = ("", port)
+        self._sock.bind(self._server_address)
+        self._sock.listen(1)
 
-        self.routes = {
+        self._routes = {
             "/": lambda: {
                 "name": self.name,
-                "routes": list(self.routes.keys()),
+                "routes": list(self._routes.keys()),
             }
         }
 
     def add_route(self, path, callback):
-        self.routes[path] = callback
+        self._routes[path] = callback
 
     def _send(self, connection, data, status=200):
-        content = json.dumps(
-            {"data" if status == 200 else "error": data},
-            default=lambda obj: str(obj),
-        )
         content = json.dumps({"data" if status == 200 else "error": data})
         connection.send(
             f"HTTP/1.1 {status} {self.HTTP_STATUS[status]}\r\nContent-Type: application/json\r\n\r\n{content}".encode()
         )
 
     def start(self):
-        print(f"Server running on port {self.server_address[1]}...")
+        print(f"Server running on port {self._server_address[1]}...")
         while True:
-            connection, client_address = self.sock.accept()
+            connection, client_address = self._sock.accept()
 
             try:
                 data = connection.recv(1024)
@@ -47,8 +44,8 @@ class MicroServer:
                 method, route, _ = lines[0].split(" ")
                 print(f"{method} {route} from {client_address[0]}")
 
-                if route in self.routes:
-                    self._send(connection, self.routes[route]())
+                if route in self._routes:
+                    self._send(connection, self._routes[route]())
                 else:
                     self._send(connection, "Route is not defined", 404)
 
