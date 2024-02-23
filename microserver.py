d0790d6ee73d0974a5bc523bdfc99bbffc40b095
Markus <kremer.markus1996@gmail.com>
2024-02-23 05:36:19 +0100

    connection details as instance var

--- microserver.py
+++ microserver.py
@@ -11,6 +11,7 @@ class MicroServer:
 
     def __init__(self, name, port=8080):
         self.name = name
+        self.connection = None
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._server_address = ("", port)
@@ -27,31 +28,33 @@ class MicroServer:
     def add_route(self, path, callback):
         self._routes[path] = callback
 
-    def _send(self, connection, data, status=200):
+    def _send(self, data, status=200):
+        if self.connection is None:
+            return
+
         content = json.dumps({"data" if status == 200 else "error": data})
-        connection.send(
+        self.connection.send(
             f"HTTP/1.1 {status} {self.HTTP_STATUS[status]}\r\nContent-Type: application/json\r\n\r\n{content}".encode()
         )
 
     def start(self):
         print(f"Server running on port {self._server_address[1]}...")
         while True:
-            connection, client_address = self._sock.accept()
+            self.connection, client_address = self._sock.accept()
 
             try:
-                data = connection.recv(1024)
+                data = self.connection.recv(1024)
                 lines = data.decode().split("\r\n")
                 method, route, _ = lines[0].split(" ")
-                print(f"{method} {route} from {client_address[0]}")
 
                 if route in self._routes:
-                    self._send(connection, self._routes[route]())
+                    self._send(self._routes[route]())
                 else:
-                    self._send(connection, "Route is not defined", 404)
+                    self._send("Route is not defined", 404)
 
             except Exception as e:
                 print(f"Error: {e}")
-                self._send(connection, e, 500)
+                self._send(e, 500)
 
             finally:
-                connection.close()
+                self.connection.close()
