9d43667a4ab04863c005107637b8e6977170541b
Markus <kremer.markus1996@gmail.com>
2024-02-24 21:13:56 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,32 +1,30 @@
 import _thread  # https://docs.micropython.org/en/latest/library/_thread.html
 from microserver import MicroServer
+from time import sleep
 
 
 # Create a server and give it a name
 server = MicroServer("Server Name")  # Default port is 8080
-server_two = MicroServer("Server Name 2", port=8081)  # NOTE: Different port
 
 # Add routes to the server
 server.add_route("/text", lambda: "Hello World!")
 server.add_route("/dict", lambda: {"hello": "world"})
-server.add_route("/tuple", lambda: ("Hello", "World!"))
 server.add_route("/list", lambda: ["Hello", "World!"])
-server.add_route("/bytes", lambda: b"Hello World!")
+
+# A server error
 server.add_route("/error", lambda: 1 / 0)
-server.add_route(
-    "/test",
-    lambda: {
-        "somekey": [b"testvalue", True],
-        "somedict": {"key": "value", "key2": False},
-    },
-)
 
-server_two.add_route("/text", lambda: "Hello World 2!")
+# nowait = don't wait for the callback to finish and immediately get a response
+server.add_route("/nowait", lambda: sleep(5), True)
 
-# Start the server...
+# Create another server with a different port
+server_two = MicroServer("Server Name 2", port=8081)
+server_two.add_route("/text", lambda: "Hello World from the second server!")
+
+# Start the servers...
 
 # ...in a new thread
 _thread.start_new_thread(server_two.start, ())
 
-# ...blocking
+# ...or blocking
 server.start()
